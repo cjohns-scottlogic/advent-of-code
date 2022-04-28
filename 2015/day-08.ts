313f51745df9ba0803754579c25ab9ac9263779c
Christopher Johns <cjohns@scottlogic.com>
2022-04-28 08:51:47 +0100

    2015: Updated day 8 solution.

--- 2015/day-08.ts
+++ 2015/day-08.ts
@@ -1,19 +1,37 @@
 import * as fs from "fs";
 import * as path from "path";
 
+const part1 = (line: string): number => {
+  var reduced = 2; // The enclosing quotes
+  var escape = false;
+
+  [...line].forEach((char) => {
+    if (escape) {
+      escape = false;
+      if (char == "\\" || char == '"') reduced += 1;
+      if (char == "x") reduced += 3;
+    } else {
+      if (char === "\\") escape = true;
+    }
+  });
+
+  return reduced;
+};
+
+const part2 = (line: string): number => {
+  var extended = 2; // The enclosing quotes
+
+  [...line].forEach((char) => {
+    if (char == "\\" || char == '"') extended += 1;
+  });
+
+  return extended;
+};
+
 fs.readFile(path.join(__dirname, "input-08.txt"), (err, data) => {
   if (err) throw err;
   var lines = data.toString().split("\r\n");
 
-  var len1 = lines.map((line) => line.length).reduce((p, c) => p + c);
-
-  var decoded = lines.map((line) => eval("String(" + line + ")"));
-  var len2 = decoded.map((line) => line.length).reduce((p, c) => p + c);
-  console.log("Part 1: " + (len1 - len2));
-
-  var encoded = lines.map(
-    (line) => '"' + line.replace(/\\/g, "\\\\").replace(/\"/g, '\\"') + '"'
-  );
-  var len3 = encoded.map((line) => line.length).reduce((p, c) => p + c);
-  console.log("Part 2: " + (len3 - len1));
+  console.log("Part 1: " + lines.map(part1).reduce((p, c) => p + c));
+  console.log("Part 2: " + lines.map(part2).reduce((p, c) => p + c));
 });
