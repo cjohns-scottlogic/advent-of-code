ce8bd3af8c27026ad80d693bffb341f3e365cfc7
Christopher Johns <cjohns@scottlogic.com>
2022-04-29 11:21:27 +0100

    2021: Day 19 at last!

--- 2021/day-19.py
+++ 2021/day-19.py
@@ -1,99 +1,68 @@
-from scipy.spatial.transform import Rotation
-from itertools import product
 from functools import partial
+from itertools import product
+from tabnanny import verbose
 
 
-class Beacon:
-    def fromString(loc):
-        return Beacon([int(x) for x in loc.split(",")])
+class Point:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
 
-    def __init__(self, xyz):
-        self.x, self.y, self.z = xyz
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
 
     def __repr__(self):
         return "{},{},{}".format(self.x, self.y, self.z)
 
+    def xyz(self):
+        return (self.x, self.y, self.z)
 
-def offset(a, b):
-    return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
-
-
-def rotate_beacon(i, beacon):
-    x, y, z = beacon.x, beacon.y, beacon.z
-    return Beacon(
-        [
-            (x, y, z),
-            (z, y, -x),
-            (-x, y, -z),
-            (-z, y, x),
-            (-x, -y, z),
-            (-z, -y, -x),
-            (x, -y, -z),
-            (z, -y, x),
-            (x, -z, y),
-            (y, -z, -x),
-            (-x, -z, -y),
-            (-y, -z, x),
-            (x, z, -y),
-            (-y, z, -x),
-            (-x, z, y),
-            (y, z, x),
-            (z, x, y),
-            (y, x, -z),
-            (-z, x, -y),
-            (-y, x, z),
-            (-z, -x, y),
-            (y, -x, z),
-            (z, -x, -y),
-            (-y, -x, -z),
-        ][i]
-    )
+    def offset(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
 
 
-rotations = [
-    Rotation.from_euler("xyz", [0, 0, 0], degrees=True),
-    Rotation.from_euler("xyz", [90, 0, 0], degrees=True),
-    Rotation.from_euler("xyz", [180, 0, 0], degrees=True),
-    Rotation.from_euler("xyz", [-90, 0, 0], degrees=True),
-    Rotation.from_euler("xyz", [0, 90, 0], degrees=True),
-    Rotation.from_euler("xyz", [90, 90, 0], degrees=True),
-    Rotation.from_euler("xyz", [180, 90, 0], degrees=True),
-    Rotation.from_euler("xyz", [-90, 90, 0], degrees=True),
-    Rotation.from_euler("xyz", [0, -90, 0], degrees=True),
-    Rotation.from_euler("xyz", [90, -90, 0], degrees=True),
-    Rotation.from_euler("xyz", [180, -90, 0], degrees=True),
-    Rotation.from_euler("xyz", [-90, -90, 0], degrees=True),
-    Rotation.from_euler("xyz", [0, 0, 90], degrees=True),
-    Rotation.from_euler("xyz", [90, 0, 90], degrees=True),
-    Rotation.from_euler("xyz", [180, 0, 90], degrees=True),
-    Rotation.from_euler("xyz", [-90, 0, 90], degrees=True),
-    Rotation.from_euler("xyz", [0, 0, 180], degrees=True),
-    Rotation.from_euler("xyz", [90, 0, 180], degrees=True),
-    Rotation.from_euler("xyz", [180, 0, 180], degrees=True),
-    Rotation.from_euler("xyz", [-90, 0, 190], degrees=True),
-    Rotation.from_euler("xyz", [0, 0, -90], degrees=True),
-    Rotation.from_euler("xyz", [90, 0, -90], degrees=True),
-    Rotation.from_euler("xyz", [180, 0, -90], degrees=True),
-    Rotation.from_euler("xyz", [-90, 0, -90], degrees=True),
-]
+class Beacon(Point):
+    def fromString(loc):
+        return Beacon(*[int(x) for x in loc.split(",")])
 
-x = []
-for r in rotations:
-    q, w, e = r.apply([[5, 7, 9]])[0]
-    p = (int(q), int(w), int(e))
-    x.append(p)
-
-for z in x:
-    q = [abs(z[0]), abs(z[1]), abs(z[2])]
-    q.sort()
-    if q != [5, 7, 9]:
-        print(q)
+    def rotate(self, i):
+        x, y, z = self.xyz()
+        return Beacon(
+            *[
+                (x, y, z),
+                (z, y, -x),
+                (-x, y, -z),
+                (-z, y, x),
+                (-x, -y, z),
+                (-z, -y, -x),
+                (x, -y, -z),
+                (z, -y, x),
+                (x, -z, y),
+                (y, -z, -x),
+                (-x, -z, -y),
+                (-y, -z, x),
+                (x, z, -y),
+                (-y, z, -x),
+                (-x, z, y),
+                (y, z, x),
+                (z, x, y),
+                (y, x, -z),
+                (-z, x, -y),
+                (-y, x, z),
+                (-z, -x, y),
+                (y, -x, z),
+                (z, -x, -y),
+                (-y, -x, -z),
+            ][i]
+        )
 
 
 class Scanner:
     def __init__(self, number):
         self.number = number
-        self.position = (0, 0, 0) if number == 0 else None
+        self.position = Point(0, 0, 0) if number == 0 else None
         self.beacons = set()
 
     def __repr__(self):
@@ -107,7 +76,8 @@ class Scanner:
         return self.position is not None
 
 
-with open("test-19.txt", "r") as f:
+with open("input-19.txt", "r") as f:
+    verbose = False
     scanners = {}
     for line in [line.strip() for line in f]:
         if len(line) > 0:
@@ -119,33 +89,22 @@ with open("test-19.txt", "r") as f:
 
     # For each scanner we're trying to match
     while True:
-        unknowns = list(filter(lambda x: not x[1].located, scanners.items()))
+        unknowns = list(filter(lambda x: not x.located, scanners.values()))
         if len(unknowns) == 0:
             break
 
         for candidate in unknowns:
-            # Try each rotation
-            # for rotation in rotations:
-            #    test = list(
-            #        map(
-            #            lambda t: (int(t[0]), int(t[1]), int(t[2])),
-            #            rotation.apply(list(candidate[1].beacons)),
-            #        )
-            #    )
             for rix in range(24):
-                beacons = set(
-                    map(
-                        partial(rotate_beacon, rix),
-                        candidate[1].beacons,
-                    )
-                )
+                beacons = set()
+                for beacon in candidate.beacons:
+                    beacons.add(beacon.rotate(rix))
 
                 # Match it against each known scanner
-                for known in filter(lambda x: x[1].located, scanners.items()):
-
+                for known in filter(lambda x: x.located, scanners.values()):
                     deltamap = {}  # Delta -> Count
+
                     # Find the differences for each beacon
-                    for kb in known[1].beacons:
+                    for kb in known.beacons:
                         for cb in beacons:
                             delta = (kb.x - cb.x, kb.y - cb.y, kb.z - cb.z)
                             if delta not in deltamap:
@@ -153,21 +112,27 @@ with open("test-19.txt", "r") as f:
                             else:
                                 deltamap[delta] += 1
 
-                    found = list(filter(lambda x: x[1] >= 12, deltamap.items()))
+                    found = list(
+                        map(
+                            lambda x: x[0],
+                            filter(lambda x: x[1] >= 12, deltamap.items()),
+                        )
+                    )
                     if len(found) > 0:
                         # We now have the offset and rotation to match this scanner
-                        delta, _ = found[0]
-                        print(
-                            "Matched scanner {} with rotation {} with known scanner {} offset {}".format(
-                                candidate[0],
-                                rix,  #  was rotations.index(rotation)
-                                known[0],
-                                delta,
+                        delta = Point(*found[0])
+                        if verbose:
+                            print(
+                                "Matched scanner {} with rotation {} with known scanner {} offset {}".format(
+                                    candidate.number,
+                                    rix,
+                                    known.number,
+                                    delta,
+                                )
                             )
-                        )
 
-                        scanner = candidate[1]
-                        scanner.position = offset(delta, known[1].position)
+                        scanner = candidate
+                        scanner.position = known.position.offset(delta)
                         scanner.beacons = beacons
                         break
 
@@ -175,13 +140,17 @@ with open("test-19.txt", "r") as f:
     for scanner in scanners.values():
         beacons = beacons.union(
             set(
-                map(
-                    partial(offset, scanner.position),
-                    map(lambda x: (x.x, x.y, x.z), scanner.beacons),
-                )
+                map(scanner.position.offset, scanner.beacons),
             )
         )
-        print(scanner)
-        print(scanner.beacons)
+    print("Part 1:", len(beacons))
 
-    print(len(beacons))
+    def manhattan(a, b):
+        return a.x - b.x + a.y - b.y + a.z - b.z
+
+    furthest = 0
+
+    for s1, s2 in product(scanners.values(), scanners.values()):
+        furthest = max(manhattan(s1.position, s2.position), furthest)
+
+    print("Part 2:", furthest)
